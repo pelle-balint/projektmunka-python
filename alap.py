ea4f59088f7507a12fa9d68bf071d1865aed7efe
pelle-balint <pelle.balint@ckik.hu>
2024-04-17 15:58:43 +0200

    Egy kis tisztítás, txt létrejövetele, csak nem véglegesen és a külön txt-k olvasási képessége, döntési lehetőséggel a user fele

--- alap.py
+++ alap.py
@@ -1,3 +1,25 @@
 from random import *
-
-print("Igen")
+import os
+def bevisz1(l, l2, l3):
+    os.system("cls")
+    f = open("igenigen.txt", "r", encoding="UTF-8")
+    f.close()
+    print('1. lefutott')
+def bevisz2(l, l2, l3):
+    os.system("cls")
+    f = open("igenigen2.txt", "r", encoding="UTF-8")
+    f.close()
+    print('2. lefutott')
+def main():
+    list, list2, list3 = [], [], []
+    x = int(input("Melyiket kéred? (1 vagy 2): "))
+    while x != 1 and x != 2:
+        os.system("cls")
+        print("Még mindig csak 1 vagy 2 az elfogadott")
+        x = int(input("Melyiket kéred? (1 vagy 2): "))
+        os.system("cls")
+    if x == 1:
+        bevisz1(list, list2, list3)
+    else:
+        bevisz2(list, list2, list3)
+main()
